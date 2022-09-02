eb41005520b4c5ba5f2644d5ae6bb9f86a9b8414
Shrey Jain <shrey.jain711@gmail.com>
2022-09-02 18:19:22 +0530

    set up post and get paths

---
+++ dbInteraction.js
@@ -0,0 +1,56 @@
+let urlDBModel = require('./models/urlDB');
+
+const dbFunctions = {
+    generateRandomCode: () => {
+        let val = "";
+        while(val.length < 6) {
+            val += String.fromCharCode(97 + Math.floor(Math.random()*26));
+        }
+        return val;
+    },
+    findUrl: (code) => {
+        let res;
+        urlDBModel.find({
+            shortUrl: code   // search query
+          })
+          .then(doc => {
+            console.log("found", doc)
+            res = doc;
+          })
+          .catch(err => {
+            console.error("findUrl error:", err);
+          })
+        return res;
+    },
+    isCodeUnique: (code) => {
+        let res = dbFunctions.findUrl(code);
+        if (!res || res.length == 0) return true;
+        return false;
+    },
+    getUniqueCode: () => {
+        let code, isUnique = false;
+        while(!isUnique){
+            code = dbFunctions.generateRandomCode();
+            isUnique = dbFunctions.isCodeUnique(code);
+        }
+        return code;
+    },
+    saveUrl: (url) => {
+        let shortUrl = dbFunctions.getUniqueCode();
+        let row = {url: url, shortUrl: shortUrl};
+        
+        let urlModel = new urlDBModel(row)
+            
+        urlModel.save()
+            .then(doc => {
+                console.log(doc);
+            })
+            .catch(err => {
+                console.error(err)
+                row = { error: 'invalid url' };
+            })
+        return row;
+    }
+}
+
+module.exports = dbFunctions;
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-require('dotenv').config();
+require('dotenv').config({path: 'C:/Users/shrey/OneDrive/Desktop/url-shorterner/keys.env'});
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dbFunctions = require('./dbInteraction');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,9 +21,17 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// POST Req
+app.post('/api/shorturl', function(req, res) {
+  let url = req.body.url;
+  res.send(JSON.stringify(dbFunctions.saveUrl(url)));
+});
+
+// GET Req
+app.get('/api/shorturl/:code', function(req, res) {
+  const code = req.params.code;
+  let url = dbFunctions.findUrl(code);
+  if (url) res.redirect(url);
 });
 
 app.listen(port, function() {
